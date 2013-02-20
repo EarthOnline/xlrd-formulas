1b5574fdac4d71b5bce404624e269936d9028a0b
EarthOnline <github@earth-online.nl>
2013-02-20 11:21:50 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+import xlrd
+
+
+setup(
+    name='xlrd-formulas',
+    packages=find_packages(),
+    include_package_data=True,
+    version=xlrd.__version__
+    description='xlrd but with formulas',
+    long_description=',
+    author=xlrd.__author__,
+    author_email='',
+    url='',
+    install_requires=[],
+    zip_safe=False,
+)
